e2f2a09b615d294a45449e23a2c7f0c08c4e007c
WladimirMihalev <winterday@tut.by>
2018-10-01 00:59:35 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false
+  }
+  let arrspl = [];
+  for (let i = 0; i < str.length; i++) 
+  {
+  for (let j = 0; j < bracketsConfig.length; j++) 
+  {
+  if (str[i] == bracketsConfig[j][0]) 
+  {
+    arrspl.push(str[i]);
+    }
+  if (str[i] == bracketsConfig[j][0]) 
+  {
+    arrspl.pop(str[i]);
+    }
+  }
+  }
+  if (arrspl == 0) 
+  {
+    return true
+  }
+  else 
+  {
+    return false
+}
 }
